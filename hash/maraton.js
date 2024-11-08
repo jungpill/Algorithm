2d46985fed1413342fd5376035f5b25cf4e4966b
이정필 <ljp1077@sungkyul.ac.kr>
2024-11-08 16:58:34 +0900

    hash 마라톤 코드 리팩토링

--- hash/maraton.js
+++ hash/maraton.js
@@ -1,40 +1,36 @@
 
-//테스트 통과 했지만 효율성에서 탈락한 방법 
-//해시 문제이지만 해시 방법을 사용하지 않음 
+//해시 map을 적용하지 않고 풀이한 방법 
 const maraton = (participant,completion) => {
-    var answer = '';
-    let i = 0;
-    participant.sort((a, b) => a.localeCompare(b));
-    completion.sort((a, b) => a.localeCompare(b));
-    
-    while(true){
-        if(completion.includes(participant[0])){
-            participant.splice(0,1)
-            completion.splice(0,1)
-        }else{
-            break
-        }
+    participant.sort();
+    completion.sort();
+    for(let i = 0 ; i < participant.length ; i++){
+        if(participant[i] !== completion[i] ){return participant[i]}
     }
     
-    return participant[0]
+    
 }
 
+//해시 개념을 사용하여 해결한 방법
 const solution = (participant, completion) => {
     const participantCount = {};
 
-    for (let p of participant) {
-        participantCount[p] = (participantCount[p] || 0) + 1;
-    }
-
-    
-    for (let c of completion) {
-        participantCount[c]--; 
-    }
-
-    
-    for (let p in participantCount) {
-        if (participantCount[p] > 0) {
-            return p; 
+    for(let i of participant){
+        if(participantCount.has(i)){
+            participantCount.set(i , participantCount.get(i) + 1)   
+        }else{
+            participantCount.set(i,1)
         }
     }
+
+    for(let i of completion){
+        if(participantCount.has(i)){
+            participantCount.set(i , participantCount.get(i) - 1)   
+        }
+    }
+
+   for(let i of participant){
+        if(participantCount.get(i) > 0){
+            return i
+        }
+   }
 }
