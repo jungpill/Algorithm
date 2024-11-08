f3afb32ac83d7d4596df46c8333c7beb3a06cd52
이정필 <ljp1077@sungkyul.ac.kr>
2024-11-08 21:32:36 +0900

    전화번호부 접두어

---
+++ phone_book.js
@@ -0,0 +1,9 @@
+//해시개념 사용 x
+
+phone_book.sort()
+    
+    for(let i = 0; i < phone_book.length - 1 ; i++){
+        if(phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)){
+            return false
+        }
+    }
