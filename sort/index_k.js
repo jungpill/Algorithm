91b7f4df65261457ccb5840d2cefc725a9bd42eb
이정필 <ljp1077@sungkyul.ac.kr>
2024-11-11 00:23:40 +0900

    k번째 수 수정

--- sort/index_k.js
+++ sort/index_k.js
@@ -1,7 +1,9 @@
-const result = []
+function solution(array, commands) {
+    const result = []
     
     for(let i = 0; i < commands.length ; i++){
-        
-result.push(array.slice(commands[i][0]-1, commands[i][1]).sort()[commands[i][2]-1])
-       
+result.push(array.slice(commands[i][0]-1, commands[i][1]).sort((a, b) => a - b)[commands[i][2]-1])
     }
+    
+    return result;
+}
