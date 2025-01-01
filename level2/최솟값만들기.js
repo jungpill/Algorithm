b8945eb68af913621f6f6dedb1d7f67b7a696bac
jungpill <ljp10777@gmail.com>
2025-01-02 01:57:00 +0900

    최솟값 만들기

---
+++ level2/최솟값만들기.js
@@ -0,0 +1,13 @@
+function solution(A,B){
+    let result = 0;
+    A.sort((a,b) => a - b);
+    B.sort((a,b) => b - a)
+    
+    for(let i = 0; i < A.length; i++){
+        result = result + (A[i]*B[i])
+    } 
+    
+    
+    return result;
+    
+}
