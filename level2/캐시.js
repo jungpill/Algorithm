b90461c1a233422e613f17b0f321ebfe2f651e76
jungpill <ljp10777@gmail.com>
2025-03-19 02:06:30 +0900

    캐시문제 해결

--- level2/캐시.js
+++ level2/캐시.js
@@ -19,4 +19,34 @@ function solution(cacheSize, cities) {
         }
     }
     return result;
-}*/
+}
+    
+해결: if(cacheMemory.length >= cacheSize) cacheMemory.shift()로 인해서 이미 배열에서 값이 한번 삭제되고 
+else if(cacheSize !== 0 && cacheMemory.includes(cities[i])) 해당 조건을 만족하며 한번 더 삭제돼서 발생하던 문제였음 
+
+*/
+
+
+function solution(cacheSize, cities) {
+    var cacheMemory = []
+    var result = 0
+    
+    if (cacheSize === 0) return cities.length * 5
+    
+    for(let i =0; i < cities.length; i++){
+        cities[i] = cities[i].toLowerCase()
+        if(cacheMemory.includes(cities[i])) result++
+        else result += 5
+        
+        if(cacheMemory.length >= cacheSize && !cacheMemory.includes(cities[i])) 
+            cacheMemory.shift()
+        
+        if(!cacheMemory.includes(cities[i])){
+            cacheMemory.push(cities[i])
+        }else{
+            cacheMemory.splice(cacheMemory.indexOf(cities[i]),1)   
+            cacheMemory.push(cities[i])
+        }
+    }
+    return result;
+}
